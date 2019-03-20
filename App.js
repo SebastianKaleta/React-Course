65df6eb6b81f22801c8534d7bad53ee33484569c
Sebastian <sebastian.m.kaleta@gmail.com>
2019-03-20 21:41:53 +0100

    First

---
+++ App.js
@@ -0,0 +1,19 @@
+// class App extends React.Component {
+//   state = {
+//     counter: 0
+//   }
+//   render() {
+//     return (
+//       <div>
+//         <h1>Hello!</h1>
+//       </div>
+//     );
+//   }
+// }
+
+// ReactDOM.render(<App />, document.getElementById('root'))
+
+console.log(React);
+console.log(ReactDOM);
+
+const element = <div> Pierwszy element React </div>;
