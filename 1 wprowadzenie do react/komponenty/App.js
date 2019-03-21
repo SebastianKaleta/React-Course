c9fedc21ea67cd51eb3376496c728b23e5a03e68
Sebastian <sebastian.m.kaleta@gmail.com>
2019-03-21 20:35:43 +0100

    First

--- 1 wprowadzenie do react/komponenty/App.js
+++ 1 wprowadzenie do react/komponenty/App.js
@@ -1,13 +1,37 @@
 //komponent funkcyjny,bezstanowy
 
-const App = () => {
-  return (
-    <div>
-      <h1>Pierwszy komponent</h1>
-    </div>
-  );
+const Header = () => {
+  return <h1>Witaj na stronie</h1>;
 };
 
 // komponent klasowy, stanowy
+
+class Blog extends React.Component {
+  state = {
+    number: 1
+  };
+  render() {
+    return (
+      <section>
+        <h2>Artykuł nr {this.state.number}</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum
+          similique hic dolore amet officiis nobis doloremque, ad molestiae
+          corrupti nemo, error inventore nisi. Nulla laborum sint quibusdam
+          veniam, blanditiis unde.
+        </p>
+      </section>
+    );
+  }
+}
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Blog />
+    </>
+  );
+};
 
 ReactDOM.render(<App />, document.getElementById("root"));
