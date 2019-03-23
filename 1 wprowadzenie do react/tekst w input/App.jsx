afe8b289a9ea73ae198ee3bb34587c9b55453c28
Sebastian <sebastian.m.kaleta@gmail.com>
2019-03-23 23:54:43 +0100

    Add reset input

--- 1 wprowadzenie do react/tekst w input/App.jsx
+++ 1 wprowadzenie do react/tekst w input/App.jsx
@@ -9,16 +9,23 @@ class App extends React.Component {
     });
   }
 
+  handleResetClick = () => {
+    this.setState({
+      value: ""
+    });
+  };
+
   render() {
     return (
       <React.Fragment>
         <input
+          value={this.state.value}
           placeholder="wpisz..."
           onChange={this.handleInputChange.bind(this)}
           type="text"
         />
-        <button>Reset</button>
-        <h1 className="title">{this.state.value}</h1>
+        <button onClick={this.handleResetClick}>Reset</button>
+        <h1 className="title">{this.state.value.toUpperCase()}</h1>
       </React.Fragment>
     );
   }
