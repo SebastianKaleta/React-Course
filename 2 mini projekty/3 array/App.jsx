bce2e936e593bf107b13e7a5751f1ce48d51930e
Sebastian <sebastian.m.kaleta@gmail.com>
2019-03-27 22:08:21 +0100

    mini-apps: work with arrays

--- 2 mini projekty/3 array/App.jsx
+++ 2 mini projekty/3 array/App.jsx
@@ -1,15 +1,59 @@
-const Item = props => <li>{`owoc ${props.content}`}</li>;
+const data = {
+  users: [
+    {
+      id: 1,
+      age: 29,
+      name: "Arek",
+      sex: "male"
+    },
+    {
+      id: 2,
+      age: 49,
+      name: "Marta",
+      sex: "female"
+    },
+    {
+      id: 3,
+      age: 19,
+      name: "Stasia",
+      sex: "female"
+    },
+    {
+      id: 4,
+      age: 30,
+      name: "Darek",
+      sex: "male"
+    },
+    {
+      id: 5,
+      age: 16,
+      name: "Lolek",
+      sex: "male"
+    }
+  ]
+};
+
+const Item = ({ user }) => (
+  <div className="userInfo">
+    <h1>Użytkownik {user.name}</h1>
+    <p>Informacje o użytkowniku</p>
+    <p>
+      Wiek użytkownika <strong> {user.age}</strong>
+    </p>
+    <p>Płeć użytkownika {user.sex}</p>
+  </div>
+);
 
 class ListItems extends React.Component {
-  state = {
-    items: ["jabłko", "śliwka", "gruszka"]
-  };
+  // state = {
+  //   items: ["jabłko", "śliwka", "gruszka"]
+  // };
   render() {
-    const Items = this.state.items.map(item => (
-      <Item key={item} content={item} />
-    ));
+    let users = this.props.data.users;
+    users = users.filter(user => user.sex === "female");
+    const Items = users.map(user => <Item key={user.id} user={user} />);
     return <ul>{Items}</ul>;
   }
 }
 
-ReactDOM.render(<ListItems />, document.getElementById("root"));
+ReactDOM.render(<ListItems data={data} />, document.getElementById("root"));
