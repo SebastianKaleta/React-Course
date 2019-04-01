bb19f2b6a6f6d4b274c3d85636f485e0066a5416
Sebastian <sebastian.m.kaleta@gmail.com>
2019-04-01 19:36:56 +0200

    mini-apps: cash exchange

--- 2 mini projekty/6 przelicznik walut/App.jsx
+++ 2 mini projekty/6 przelicznik walut/App.jsx
@@ -1,51 +1,83 @@
 const Cash = props => {
+  const value = ((props.cash / props.ratio) * props.prices).toFixed(2);
   return (
     <div>
       {props.title}
-      {props.cash <= 0 ? "" : (props.cash / props.ratio).toFixed(2)}
+      {props.cash <= 0 ? "" : value}
     </div>
   );
 };
 
 class ExchangeCounter extends React.Component {
   state = {
-    amount: ""
-    // ratioDollar: 3.6,
-    // ratioEuro: 4.2
+    amount: "",
+    product: "gas"
   };
+  static defaultProps = {
+    currencies: [
+      { id: 0, name: "zloty", ratio: 1, title: "Wartość w złotówkach:" },
+      { id: 1, name: "dollar", ratio: 3.6, title: "Wartość w dolarach:" },
+      { id: 2, name: "euro", ratio: 4.2, title: "Wartość w euro:" },
+      { id: 3, name: "pound", ratio: 4.55, title: "Wartość w funtach:" }
+    ],
 
-  currences = [
-    { id: 1, name: "dollar", ratio: 3.6, title: "Wartość w dolarach:" },
-    { id: 2, name: "euro", ratio: 4.2, title: "Wartość w euro:" },
-    { id: 3, name: "pound", ratio: 4.55, title: "Wartość w funtach:" }
-  ];
-
+    prices: { electricity: 0.51, gas: 4.76, oranges: 3.79 }
+  };
   handleChange = e => {
     this.setState({
       amount: e.target.value
     });
   };
+  handleSelect = e => {
+    this.setState({
+      product: e.target.value,
+      amount: ""
+    });
+  };
+
+  insertSuffix(select) {
+    if (select === "electricity") return <em>kWh</em>;
+    else if (select === "gas") return <em>litrów</em>;
+    else if (select === "oranges") return <em>kilogramów</em>;
+    else return null;
+  }
+
+  selectPrices(select) {
+    return this.props.prices[select];
+  }
 
   render() {
-    const { amount } = this.state; //destrukturyzacja
+    const { amount, product } = this.state; //destrukturyzacja
+    const price = this.selectPrices(product);
 
-    const calculators = this.currences.map(currency => (
+    const calculators = this.props.currencies.map(currency => (
       <Cash
         key={currency.id}
         ratio={currency.ratio}
         title={currency.title}
         cash={amount}
+        prices={price}
       />
     ));
 
     return (
       <div className="app">
         <label>
+          Wybierz produkt:
+          <select value={product} onChange={this.handleSelect}>
+            <option value="electricity">prąd</option>
+            <option value="gas">benzyna</option>
+            <option value="oranges">pomarańcze</option>
+          </select>
+        </label>
+        <br />
+        <label>
           <input
-            type="nunmber"
+            type="number"
             value={this.state.amount}
             onChange={this.handleChange}
           />
+          {this.insertSuffix(this.state.product)}
         </label>
         {calculators}
       </div>
