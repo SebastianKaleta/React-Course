dbcc5a8db059dfb6d386b88cafd3a1b7033370bf
Sebastian <sebastian.m.kaleta@gmail.com>
2019-04-02 21:54:52 +0200

    mini-apps: delete list item

--- 2 mini projekty/8 zadanie usuwanie z listy/App.jsx
+++ 2 mini projekty/8 zadanie usuwanie z listy/App.jsx
@@ -1,8 +1,21 @@
+//pierwszy sposób
+
+// const Person = props => {
+//   return (
+//     <li>
+//       <span>{props.name}</span>
+//       <button onClick={props.delete}>usuń</button>
+//     </li>
+//   );
+// };
+
+//trzeci sposób
+
 const Person = props => {
   return (
     <li>
       <span>{props.name}</span>
-      <button onClick={props.delete}>usuń</button>
+      <button onClick={() => props.delete(props.name)}>usuń</button>
     </li>
   );
 };
@@ -17,6 +30,8 @@ class List extends React.Component {
     ]
   };
 
+  //pierwszy sposób
+
   // handleDelete(id) {
   //   const people = [...this.state.people];//tworzenie kopii tablicy
   //   const index = people.findIndex(person => person.id === id);
@@ -26,29 +41,49 @@ class List extends React.Component {
   //   });
   // }
 
-  handleDelete(name) {
-    // let people = Array.from(this.state.people);//inny sposób na tworzenie kopii tablicy
-    let people = this.state.people.slice(); //to co wyżej, na inny sposób
+  //drugi sposób
 
-    people = people.filter(person => name !== person.name);
+  // handleDelete(name) {
+  //   // let people = Array.from(this.state.people);//inny sposób na tworzenie kopii tablicy
+  //   let people = this.state.people.slice(); //to co wyżej, na inny sposób
+
+  //   people = people.filter(person => name !== person.name);
+  //   this.setState({
+  //     people
+  //   });
+  // }
+
+  //trzeci sposób
+  handleDelete = id => {
+    const people = [...this.state.people]; //tworzenie kopii tablicy
+    const index = people.findIndex(person => person.id === id);
+    people.splice(index, 1);
     this.setState({
       people
     });
-  }
+  };
 
   render() {
+    //pierwszy sposób
+
     // const people = this.state.people.map(person => (
     //   <Person
     //     key={person.id}
     //     name={person.name}
     //     delete={this.handleDelete.bind(this, person.id)}
     //   />
+
+    //drugi sposób
+    // const people = this.state.people.map(person => (
+    //   <Person
+    //     key={person.id}
+    //     name={person.name}
+    //     delete={this.handleDelete.bind(this, person.name)}
+    //   />
+
+    //trzeci sposób
     const people = this.state.people.map(person => (
-      <Person
-        key={person.id}
-        name={person.name}
-        delete={this.handleDelete.bind(this, person.name)}
-      />
+      <Person key={person.id} name={person.name} delete={this.handleDelete} />
     ));
     return (
       <div>
