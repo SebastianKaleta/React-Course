09a4016e07f0affe60f8dfbd22b78a7b402de5b5
Sebastian <sebastian.m.kaleta@gmail.com>
2019-04-28 22:31:36 +0200

    Spa projects starting files

---
+++ 5-React-Router/SPA/2-spa/src/layouts/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../styles/App.css";
+import { BrowserRouter as Router } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <div>SPA</div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
