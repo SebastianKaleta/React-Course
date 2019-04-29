a6d2ace39129371b918041410bd30942adcba163
Sebastian <sebastian.m.kaleta@gmail.com>
2019-04-29 22:30:39 +0200

    Spa project navigation

---
+++ 5-React-Router/SPA/2-spa/src/layouts/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+const Footer = () => {
+  return (
+    <div>
+      footer <div>2</div>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ 5-React-Router/SPA/2-spa/src/layouts/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+const Header = () => {
+  return (
+    <div>
+      Header
+      <div>2</div>
+    </div>
+  );
+};
+
+export default Header;
