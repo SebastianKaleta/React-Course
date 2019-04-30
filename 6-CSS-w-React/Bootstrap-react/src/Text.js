fcf2f35c6b00aaa072f2e214a3ebc3c95bd955ad
Sebastian <sebastian.m.kaleta@gmail.com>
2019-04-30 15:13:04 +0200

    bootstrap css in react

---
+++ 6-CSS-w-React/Bootstrap-react/src/Text.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Text.css";
+
+const Text = () => {
+  return (
+    <div>
+      <button className="btn btn-dark">Przycisk</button>
+      <p className="red">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, ex
+        et sit eius mollitia quam obcaecati. Doloribus amet dicta perferendis
+        maiores nesciunt quidem eveniet laboriosam reiciendis ex! Earum,
+        doloremque adipisci.
+      </p>
+    </div>
+  );
+};
+
+export default Text;
