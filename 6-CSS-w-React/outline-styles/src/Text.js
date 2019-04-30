50ebd7f1ba460a9db298ed97cd6903f6d2ad2e6f
Sebastian <sebastian.m.kaleta@gmail.com>
2019-04-30 14:59:52 +0200

    external css in react

---
+++ 6-CSS-w-React/outline-styles/src/Text.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Text.css";
+
+const Text = () => {
+  return (
+    <p className="red">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, ex
+      et sit eius mollitia quam obcaecati. Doloribus amet dicta perferendis
+      maiores nesciunt quidem eveniet laboriosam reiciendis ex! Earum,
+      doloremque adipisci.
+    </p>
+  );
+};
+
+export default Text;
